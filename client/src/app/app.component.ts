e491807873f65fcac019bfc0a54c48c25fde405f
TanvirAhmed1 <tanvirnoor38@yahoo.com>
2022-01-20 00:50:20 +0600

    login functionality added to client

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { User } from './_models/user';
+import { AccountService } from './_services/account.service';
 
 @Component({
   selector: 'app-root',
@@ -7,14 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
-  title = 'Notes Management System';
+  title = 'Notes Manager';
   users: any;
-  constructor(private http: HttpClient){}
+  constructor(private http: HttpClient, private accountService: AccountService){}
   ngOnInit(){
     this.getUsers();
+    this.setCurrentUser();
+  }
+  setCurrentUser(){
+    const user: User = JSON.parse(localStorage.getItem('user'));
+    this.accountService.setCurrentUser(user);
   }
   getUsers(){
-    this.http.get('https://localhost:44310/api/users').subscribe(response =>{
+    this.http.get('https://localhost:44302/api/users').subscribe(response =>{
       this.users = response;
     }, error => {
       console.log(error);
